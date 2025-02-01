61eeacbead6267c51d050499789065beb386eb75
Dmitry Rybakov <dima.dr.2001@gmail.com>
2025-02-01 13:12:19 +0100

    feat: add rinunciaBorsaTemplate

---
+++ app/lib/templates/rinunciaBorsaTemplate.ts
@@ -0,0 +1,16 @@
+export function rinunciaBorsaTemplate({
+  rinunciatario,
+  numeroProtocollo,
+  numeroBando,
+  responsabileScientifico,
+  motivoRinuncia,
+}: {
+  rinunciatario: string;
+  numeroProtocollo: string;
+  numeroBando: string;
+  responsabileScientifico: string;
+  motivoRinuncia: string;
+}) {
+  return `\\subsection{Rinuncia dott. ${rinunciatario}}
+Il Direttore comunica che con nota protocollo ${numeroProtocollo} il dott. ${rinunciatario} ha dichiarato di voler rinunciare all'assegno di ricerca su xxxxxxx emanato con DR ${numeroBando} del xx.xx.2xx, in quanto ${motivoRinuncia}. A tale scopo consegna la relazione conclusiva delle attività svolte (Allegato x) congiuntamente al parere del responsabile scientifico prof. ${responsabileScientifico} (Allegato x). Il Consiglio inanime approva`;
+}
