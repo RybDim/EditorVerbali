6405d71444abf927694a0fad9aa7b1b049a04619
Dmitry Rybakov <dima.dr.2001@gmail.com>
2025-02-14 16:31:01 +0100

    feat(auth): add validation schemas for login and registration

---
+++ lib/schemas/register_schema.ts
@@ -0,0 +1,14 @@
+import * as z from "zod";
+
+export const RegisterSchema = z.object({
+	name: z.string(),
+	email: z.string().email(),
+	password: z.string().min(8, {
+		message: "La password deve contenere almeno 8 caratteri",
+	}),
+	confirm_password: z.string()
+})
+.refine((data) => data.password !== data.confirm_password, {
+	message: "Password non corrisponde",
+	path: ["conferma"],
+});
