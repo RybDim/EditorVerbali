c05b5ab66e0ea9ccf6371e156cca73a5da5915bc
Dmitry Rybakov <dima.dr.2001@gmail.com>
2025-02-17 17:59:17 +0100

    refactor(utils): move generateVerbale from /editor/page.tsx to utils.ts and add formatDate utility

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,39 @@
+import { FormValues } from "@/types/types";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { template } from "./template";
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
+}
+
+export function formatDate(dateString: string) {
+	return new Date(dateString).toLocaleDateString('it-IT', {
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric'
+	})
+}
+
+export async function generateVerbale(formData: FormValues): Promise<string> {
+	const texDoc = template(formData);
+	try {
+		const response = await fetch("/api/compile", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ tex_string: texDoc }),
+		});
+
+		if (!response.ok) {
+			throw new Error("Pdf generation failed");
+		}
+
+		const data = await response.json();
+		return data.pdf;
+	} catch (error) {
+		console.log("Error generating pdf: ", error);
+		throw error;
+	}
 }
